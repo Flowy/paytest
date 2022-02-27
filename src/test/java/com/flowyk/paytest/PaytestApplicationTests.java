633a68d2ef790574b91e262e811ffd5fe399772f
Flowy <flowyk@gmail.com>
2022-02-27 14:07:15 +0100

    Initial commit

---
+++ src/test/java/com/flowyk/paytest/PaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowyk.paytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
